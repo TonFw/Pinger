33a2b2e39008042404a66bab19d06719428a54fe
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-30 21:55:12 -0200

    routes using nested

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  # Just base CRUD nested resources
+  resources :targets do
+    resources :schedules do
+      resources :pings
+    end
+  end
+
   # Root request
   root to: "home#index"
 
-  # CRUD Resources
-  resources :pings
-  resources :targets
-  resources :schedules
-
+  # CRUD Resources  resources :targets
   # Devise session manager
   devise_for :users
 end
