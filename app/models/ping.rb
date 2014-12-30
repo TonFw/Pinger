329b3ad7237398223e8c31d7bf8d631540e96406
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2014-12-30 20:00:54 -0200

    models validations added

--- app/models/ping.rb
+++ app/models/ping.rb
@@ -1,4 +1,13 @@
 class Ping < ActiveRecord::Base
-  belongs_to :scheduled
+  # Associations
+  belongs_to :schedule
   belongs_to :target
+
+  # Rails validations
+  validates :http_code, numericality: {}
+  validates_inclusion_of :http_code, in: HttpConst.codes.values, on: [:create, :update]
+
+  # Association validations
+  validates_presence_of :schedule_id
+  validates_presence_of :target_id
 end
--- app/models/target.rb
+++ app/models/target.rb
@@ -1,3 +1,11 @@
 class Target < ActiveRecord::Base
+  # Associations
   belongs_to :user
+  has_many :schedules
+
+  # Rails validations
+  validates :url, presence: true, length: { in: 10..255 }, on: [:create, :update]
+
+  # Associations validations
+  validates_presence_of :user_id, on: [:create, :update]
 end
