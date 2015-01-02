344f07d42ce870775f844d2a0689bc69b5567f8f
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2015-01-02 11:25:35 -0200

    name method created

--- app/models/target.rb
+++ app/models/target.rb
@@ -8,4 +8,23 @@ class Target < ActiveRecord::Base
 
   # Associations validations
   validates_presence_of :user_id, on: [:create, :update]
+
+  # Return it name
+  def name
+    # Base vars
+    aux_name = self.url
+    http_index = aux_name.index('http://')
+    https_index = aux_name.index('https://') if http_index.nil?
+
+    # SetUp if http or https
+    if !http_index.nil?
+      protocol = 'http://'
+      protocol_index = http_index
+    else
+      protocol = 'https://'
+      protocol_index = https_index
+    end
+
+    aux_name[protocol_index+protocol.length..url.index('.')-1]
+  end
 end
