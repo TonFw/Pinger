359836173626e7d5e112297b1e3a5981ce0bb8d9
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2015-01-03 12:20:29 -0200

    Using the devise user mocked helper

--- spec/requests/targets_spec.rb
+++ spec/requests/targets_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe "Targets" do
+  # Runs before each test.
+  before do
+    # Sign in as a user.
+    sign_in_as_a_valid_user
+  end
+
   describe "GET /targets" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
