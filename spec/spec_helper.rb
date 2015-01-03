ba95875f6baacc998d7b8aef2da770803ec331c9
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2015-01-03 12:03:13 -0200

    requiring devise

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ end
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
+require 'devise'
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'factory_girl_rails'
