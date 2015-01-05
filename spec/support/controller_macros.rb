d4a3e0b2319b3b76799f473d6b5ce917f3b90b6e
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2015-01-05 15:23:08 -0200

    - confirm commented (it is only if use it on the devise to confirm the user; - @user instantiated to be retrieved on the tests (it was without the @).

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -2,9 +2,8 @@ module ControllerMacros
   def login_user
     before(:each) do
       @request.env['devise.mapping'] = Devise.mappings[:user]
-      user = FactoryGirl.create(:user)
-      # user.confirm! # only necessary if using confirmable module
-      sign_in user
+      @user = FactoryGirl.create(:user) # user.confirm! # only necessary if using confirmable module
+      sign_in @user
     end
   end
 end
