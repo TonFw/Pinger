fd6ae986438b11663279dd12ab7a72386e9ba5df
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2015-01-05 16:11:20 -0200

    testing schedule unique per target

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Schedule do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Unique schedule per Target' do
+    # SetUp the Objs
+    before do
+      # base vars
+      @user = FactoryGirl.create(:user)
+      @target = FactoryGirl.build(:target)
+      @schedule = FactoryGirl.build(:schedule)
+      @schedule2 = FactoryGirl.build(:schedule)
+
+      # Associate & save it
+      @target.user = @user
+      @target.save
+      @schedule.target = @target
+      @schedule.save
+    end
+
+    it 'should not be saved' do
+      @schedule2.target = @target
+      expect(@schedule2.save).to_not be_true
+      expect(@schedule2.errors.messages).to_not be_empty
+    end
+  end
 end
