32bbd2a73f2519a536f4895ec577e7bb338f3bd2
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2015-01-05 16:58:48 -0200

    pings requests

--- spec/requests/pings_spec.rb
+++ spec/requests/pings_spec.rb
@@ -5,14 +5,24 @@ describe "Pings" do
   before do
     # Sign in as a user.
     sign_in_as_a_valid_user
-    @target_id = 1
-    @schedule_id = 1
+
+    # Nested objs
+    @schedule = FactoryGirl.build(:schedule)
+    @target = FactoryGirl.build(:target)
+
+    # setup the target
+    @target.user = @user
+    @target.save
+
+    # setup the schedule
+    @schedule.target = @target
+    @schedule.save
   end
 
   describe "GET /targets/:target_id/schedules/:schedule_id/pings" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get target_schedule_pings_path(@target_id, @schedule_id)
+      get target_schedule_pings_path(@target.id, @schedule.id)
       expect(response.status).to eq 200
     end
   end
