04cc9f73e5b72e1732ef1c6e102b1b4a34b3e4d1
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2015-01-05 10:45:28 -0200

    more readable code

--- app/models/ping.rb
+++ app/models/ping.rb
@@ -1,13 +1,13 @@
 class Ping < ActiveRecord::Base
   # Associations
-  belongs_to :schedule
   belongs_to :target
+  belongs_to :schedule
 
   # Rails validations
   validates :http_code, numericality: {}
   validates_inclusion_of :http_code, in: HttpConst.codes.values, on: [:create, :update]
 
   # Association validations
-  validates_presence_of :schedule_id
   validates_presence_of :target_id
+  validates_presence_of :schedule_id
 end
