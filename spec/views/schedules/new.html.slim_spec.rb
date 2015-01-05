53fa90ffb441ebcbac34746c25f304c605bdac70
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2015-01-05 15:21:42 -0200

    form action assertive updated

--- spec/views/schedules/new.html.slim_spec.rb
+++ spec/views/schedules/new.html.slim_spec.rb
@@ -12,7 +12,7 @@ describe "schedules/new" do
     render
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form[action=?][method=?]", schedules_path, "post" do
+    assert_select "form[action=?][method=?]", target_schedules_path, "post" do
       assert_select "input#schedule_hour[name=?]", "schedule[hour]"
       assert_select "input#schedule_target[name=?]", "schedule[target]"
     end
