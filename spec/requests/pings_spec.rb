187d0353d8c29e8e2a79c053348193f5183bd576
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2015-01-05 10:48:03 -0200

    devise login wih helper/support added

--- spec/requests/pings_spec.rb
+++ spec/requests/pings_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe "Pings" do
+  # Runs before each test.
+  before do
+    # Sign in as a user.
+    sign_in_as_a_valid_user
+  end
+
   describe "GET /pings" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
--- spec/requests/schedules_spec.rb
+++ spec/requests/schedules_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 describe "Schedules" do
-  describe "GET /schedules" do
+  # Runs before each test.
+  before do
+    # Sign in as a user.
+    sign_in_as_a_valid_user
+  end
+
+  describe "GET /targets/:target_id/schedules" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get schedules_path
+      get target_schedules_path(1)
       response.status.should be(200)
     end
   end
